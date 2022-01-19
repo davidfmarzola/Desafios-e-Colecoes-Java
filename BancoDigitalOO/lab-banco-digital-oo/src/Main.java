08165dfa9e6d5795dfcc08f89860fc86fd9614cd
David Freitas <dvdcomputacao@gmail.com>
2022-01-19 09:36:33 -0300

    Update Main.java

--- BancoDigitalOO/lab-banco-digital-oo/src/Main.java
+++ BancoDigitalOO/lab-banco-digital-oo/src/Main.java
@@ -1,18 +1,61 @@
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+	public static void tratarOperacoes(Conta objetoTipoConta, String operacao, double valor) {
+		if(operacao.compareTo("Saque") == 0) {
+			objetoTipoConta.sacar(valor);
+		} else if(operacao.compareTo("Depósito") == 0) {
+			objetoTipoConta.depositar(valor);
+		}
+		objetoTipoConta.imprimirExtrato();
+	}
+	
+	public static void tratarTransferencia(Conta contaEnvio, Conta contaDestino, double valor) {
+		//transfiro da contaEnvio para a contaDestino
+		contaEnvio.transferir(valor, contaDestino);
+		contaEnvio.imprimirExtrato();
+	}
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		cc.imprimirExtrato();
+	public static void main(String[] args) throws InterruptedException, IOException {
+		Scanner scan = new Scanner(System.in);
 		
-		poupanca.imprimirExtrato();
+		System.out.println("Nome: ");
+		String nome = scan.next();
+		Cliente c = new Cliente();
+		c.setNome(nome);
+		
+		System.out.println("=== Corrente ou Poupança ===");
+		System.out.print("Conta: ");
+		String conta = scan.next();
+		
+		System.out.println("=== Saque, Depósito ou Transferência ===");
+		System.out.print("Operação: ");
+		String operacao = scan.next();
+		
+		System.out.print("Valor R$");
+		double valor = scan.nextInt();
+		Conta cc = new ContaCorrente(c);
+		Conta poupanca = new ContaCorrente(c);
+		if(operacao.compareTo("Transferência") == 0) {
+			if(conta.compareTo("Corrente") == 0)
+				tratarTransferencia(cc, poupanca, valor);
+			else 
+				tratarTransferencia(poupanca, cc, valor);
+			
+		} else if(operacao.compareTo("Transferência") != 0 && conta.compareTo("Poupança") == 0) {
+			//metódo que trata da operação e do valor para determinada conta
+			tratarOperacoes(poupanca, operacao, valor);
+			
+		}else if(operacao.compareTo("Transferência") != 0 && conta.compareTo("Corrente") == 0) {
+			tratarOperacoes(cc, operacao, valor);
+			
+		} else {
+			System.out.println("Conta e/ou Operação inválida! Tente novamente.");
+			String[] m = new String[] {}; 
+			Main.main(m);
+		}
 		
 	}
 
